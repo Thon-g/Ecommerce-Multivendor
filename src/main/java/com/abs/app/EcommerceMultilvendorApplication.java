f1646d33140282c491f180d736368a7fcf8d7c92
Thon-g <minhphongth06@gmail.com>
2026-08-02 18:16:50 +0700

    Initial commit

---
+++ src/main/java/com/abs/app/EcommerceMultilvendorApplication.java
@@ -0,0 +1,13 @@
+package com.abs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMultilvendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMultilvendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abs/app/EcommerceMultilvendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMultilvendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
