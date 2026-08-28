759705f7f150200163722ae59edc10f8270524f9
Thon-g <minhphongth06@gmail.com>
2026-08-28 11:05:37 +0700

    feat(common-foundation): add @Service from FileStorageService

--- src/main/java/com/abs/app/infrastructure/file/FileStorageService.java
+++ src/main/java/com/abs/app/infrastructure/file/FileStorageService.java
@@ -1,5 +1,6 @@
 package com.abs.app.infrastructure.file;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+@Service
 public class FileStorageService {
     private static final String AVATAR_UPLOAD_DIR = System.getProperty("user.dir") + "/images/uploads/avatars/";
     private static final String AVATAR_PUBLIC_PATH = "/images/uploads/avatars/";
