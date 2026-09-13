5c218de2cc34ed49e2f339842f3a3aee14169c32
Thon-g <minhphongth06@gmail.com>
2026-09-13 14:04:54 +0700

    fix(order): remove redundant /api prefix from OrderController due to global context-path

--- src/main/java/com/abs/app/presentation/controller/user/OrderController.java
+++ src/main/java/com/abs/app/presentation/controller/user/OrderController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
