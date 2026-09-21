f8cbe9e96cc38756d057fbdbcd2e73e8510b116d
Thon-g <minhphongth06@gmail.com>
2026-09-21 11:17:20 +0700

    fix(ci): remove orphaned cartItemRepository.save call causing compilation error

--- src/test/java/com/abs/app/integration/CheckoutIntegrationTest.java
+++ src/test/java/com/abs/app/integration/CheckoutIntegrationTest.java
@@ -127,7 +127,6 @@ public class CheckoutIntegrationTest extends BaseIntegrationTest {
         cartItem.setSellingPrice(100);
         cartItem.setMrpPrice(120);
         cartItem.setUserId(user.getUserId());
-        cartItemRepository.save(cartItem);
 
         Cart cart = new Cart();
         cart.setUser(user);
